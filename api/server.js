660d1d919f6a578643cd0374e57bf997a45915f2
Avanishdev <avanishporwal01@gmail.com>
2024-08-13 03:07:12 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -21,11 +21,11 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build', 'index.html'));
+// });
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
